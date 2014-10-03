23aded3b4cb9ce52da8e42026d7a12e2e322cb79
Nick Mitchell <nick@superlogical.org>
2014-10-03 15:26:33 +1300

    Overwrote database.config to be a template file with handlebars syntax

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'laravel_scotchio',
-			'username'  => 'root',
-			'password'  => 'mys092500',
+			'database'  => '{{database}}',
+			'username'  => '{{username}}',
+			'password'  => '{{password}}',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
